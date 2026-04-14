5b76ea03c79b1ecfb87f1aff8d0bb95e5b78a823
Sadman Sakib Abesh <sadmansakibabesh@gmail.com>
2026-04-14 07:40:48 +0600

    Refactor GPIO functions for target port selection
    Refactored GPIO functions to use a target function for port selection.

--- GPIO_Function/gpio.c
+++ GPIO_Function/gpio.c
@@ -1,14 +1,8 @@
 #include "stm32f411xe.h"
 #include "gpio.h"
 
-
-void gpio_init(char port){
-	RCC->AHB1ENR |=(1<<port);
-}
-
-
-void pinOutput(char port,char no){
-	GPIO_TypeDef *target_port;
+GPIO_TypeDef *target(char port){
+GPIO_TypeDef *target_port;
 
 switch (port) {
 
@@ -40,6 +34,17 @@ switch (port) {
         target_port = GPIOA; // fallback (or error handler)
         break;
 }
+
+return target_port;
+}
+void gpio_init(char port){
+	RCC->AHB1ENR |=(1<<port);
+}
+
+
+void pinOutput(char port,char no){
+	GPIO_TypeDef *target_port=target(port);
+
 
 
 target_port->MODER &= ~(0b11<<no*2);
@@ -48,39 +53,35 @@ target_port ->OTYPER &= ~(0b1<<no);
 
 }
 
-void pin(char port,char no, char mode){
-	GPIO_TypeDef *target_port;
+void pinInput(char port,char no){
+	GPIO_TypeDef *target_port=target(port);
 
-switch (port) {
 
-    case PortA:
-        target_port = GPIOA;
-        break;
+  target_port ->MODER &= ~(0b11<<no*2);
+   target_port ->MODER |= (0b00<<no*2);
+   target_port ->OTYPER &= ~0b1<<no;
 
-    case PortB:
-        target_port = GPIOB;
-        break;
+   
+   target_port ->PUPDR &= ~(0b11<<no*2);
+   target_port ->PUPDR |= (0b10<<no*2);
 
-    case PortC:
-        target_port = GPIOC;
-        break;
-
-    case PortD:
-        target_port = GPIOD;
-        break;
-
-    case PortE:
-        target_port = GPIOE;
-        break;
-
-    case PortH:
-        target_port = GPIOH;
-        break;
-
-    default:
-        target_port = GPIOA; // fallback (or error handler)
-        break;
 }
+
+
+
+
+int status(char port, char no){
+GPIO_TypeDef *target_port=target(port);
+
+return (target_port->IDR & (1 << no)) ? 1 : 0;
+
+}
+
+
+
+void pin(char port,char no, char mode){
+	GPIO_TypeDef *target_port=target(port);
+
 
 
 if(mode==1){target_port ->ODR|=(0b1<<no);}
