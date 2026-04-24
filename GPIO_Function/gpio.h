b86a979db8afe9b8453107dc5d7538ecf843dac5
Sadman Sakib Abesh <sadmansakibabesh@gmail.com>
2026-04-24 09:41:42 +0600

    Add pinAf and pinADC functions to gpio.h

--- GPIO_Function/gpio.h
+++ GPIO_Function/gpio.h
@@ -19,5 +19,7 @@
 void gpio_init(char port);
 void pinOutput(char port,char no);
 void pinInput(char port,char no);
+void pinAf(char port,char no);
 int status(char port, char no);
 void pin(char port,char no, char mode);
+void pinADC(char port, char no);
